13f679670e9a993cf8ad6cb81996c66baa21031d
ganow <y.nagano.92@gmail.com>
2013-07-15 23:35:25 +0900

    cleaning

--- client.c
+++ client.c
@@ -43,39 +43,6 @@ int main(int argc, char const *argv[])
 
     PrintCompanies(companies);
 
-    // for (int i = 0; i < 22; i++) {
-    //     printf("read num: %u\n", buf[i]);
-    //     int num = ntohl(buf[i]);
-    //     printf("num = %u\n", num);
-    // }
-
-    // /* 送られてきたkeyに学籍番号を足して送信 */
-    // num += 71146957;
-    // num = htonl(num);
-    // len = -1;
-    // while (len == -1) {
-    //   len = write(fd, &num, sizeof(num));
-    // }
-
-    // /* 学籍番号を送信 */
-    // len = -1;
-    // num = htonl(71146957);
-    // while (len == -1) {
-    //   len = write(fd, &num, sizeof(num));
-    // }
-
-    // /* うまく行ったかどうかの確認 */
-    // len = -1;
-    // while (len == -1) {
-    //   len = read(fd, &buf, BUFSIZE);
-    // }
-    // num = ntohl(buf);
-    // if (num == 0x41434345) {
-    //   printf("accepted!!\n");
-    // } else {
-    //   printf("failed!!\n");
-    // }
-
     close(fd);
 
     return 0;
